fa59ac455be6fdf30a6cbb13492d7f6676488275
Juan Antonio Ramírez Puello <73839170+SoulSolidus@users.noreply.github.com>
2021-10-11 19:13:07 -0400

    Add files via upload

--- JS/scroll2.js
+++ JS/scroll2.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         scroll = $(window).scrollTop();
 
         if(scroll > 100){
-            $(".nav").css({"background-color": "#2A3D45", "transition": "0.7s ease"});
+            $(".nav").css({"background-color": "#00b4d8", "transition": "0.7s ease"});
             $(".nav").css({"position": "sticky", "transition": "0.7s ease"});
         } else{
             $(".nav").css({"background-color": "transparent"});
